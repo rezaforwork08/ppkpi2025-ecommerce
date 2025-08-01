7fc4f42a7c43453ce8db2ea59ff40804d00d3bbd
rezaforwork08 <rezaforwork08@gmail.com>
2025-08-01 11:40:41 +0700

    add get_total_cart

--- content/tes.php
+++
@@ -1,1 +0,0 @@
-<h1>DUarr tess</h1>
--- inc/header.php
+++ inc/header.php
@@ -64,7 +64,7 @@
                   <div class="header__cart">
                       <ul>
                           <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
-                          <li><a href="#"><i class="fa fa-shopping-bag"></i> <span class="total_item">0</span></a></li>
+                          <li><a href="?pg=cart"><i class="fa fa-shopping-bag"></i> <span class="total_item">0</span></a></li>
                       </ul>
                       <div class="header__cart__price">item: <span>$150.00</span></div>
                   </div>
